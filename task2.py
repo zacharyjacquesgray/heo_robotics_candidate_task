d1b8d923b9a64a14f9951a9a192832fd1eb075e1
Zachary Gray <zgra6187@uni.sydney.edu.au>
2023-07-21 23:52:40 +1000

    Fixed plot to show months

--- task2.py
+++ task2.py
@@ -1,56 +1,60 @@
 import json
+import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.dates import MonthLocator, DateFormatter
 from datetime import datetime
 
-file_path = "iss_elset.json"
+file_path = "iss_elset.json"  # elset data as json downloaded from space-track.org 
 
 with open(file_path, "r") as json_file:
     data = json.load(json_file)
 
+# Relevant data initialisations for altitude data and its associated timestamp
 semi_major_axis_array = []
 eccentricity_array = []
-timestamps = [] # for first and last time stamps in dataset
+timestamps = []
 
-# Extract relevant data to calculate altitude from ELSET data.
+# Extract relevant data to calculate altitude from ELSET json
 for item in data:
     semi_major_axis_array.append(float(item["SEMIMAJOR_AXIS"]))
     eccentricity_array.append(float(item["ECCENTRICITY"]))
-    timestamps.append(item["EPOCH"])  # Extracting timestamps from the data
+    timestamps.append(item["EPOCH"])
 
-print("Semi-Major Axes:", semi_major_axis_array)
-print("Eccentricities:", eccentricity_array)
+earth_radius = 6371  # kilometres
 
-earth_radius = 6371  # kilometers
-
+# Calculate altitudes over the year
 altitude_array = []
 # Altitude = (semimajor_axis * (1 - eccentricity) - earth_radius)
 for semi_major_axis, eccentricity in zip(semi_major_axis_array, eccentricity_array):
     altitude_array.append(semi_major_axis * (1 - eccentricity) - earth_radius)
 
-print("Altitudes:", altitude_array)
-
-time_array = range(len(altitude_array))
-
 # Convert timestamps to datetime objects
 timestamps_datetime = [datetime.fromisoformat(ts) for ts in timestamps]
 
-# Find the earliest and latest timestamps
+# Find the earliest and latest datetimes obtained
 earliest_timestamp = min(timestamps_datetime)
 latest_timestamp = max(timestamps_datetime)
 
-# Format the earliest and latest timestamps for display
-earliest_date_time = earliest_timestamp.strftime("%Y-%m-%d %H:%M:%S")
-latest_date_time = latest_timestamp.strftime("%Y-%m-%d %H:%M:%S")
+# Print dates obtained to console.
+earliest_date_time = earliest_timestamp.strftime("%d-%m-%Y %H:%M:%S")
+latest_date_time = latest_timestamp.strftime("%d-%m-%Y %H:%M:%S")
+print("Datetime of the earliest data obtained:", earliest_date_time)
+print("Datetime of the latest data obtained:", latest_date_time)
 
-print("Date and time of the earliest set:", earliest_date_time)
-print("Date and time of the latest set:", latest_date_time)
-
+# Set time array
+time_array = np.linspace(earliest_timestamp.timestamp(), latest_timestamp.timestamp(), len(altitude_array))
 
 # Plot the altitude
 plt.figure()
-plt.plot(time_array, altitude_array)
-plt.xlabel('Time')
+plt.plot(timestamps_datetime, altitude_array)
+plt.xlabel('21 July 2022 - 21 July 2023')
 plt.ylabel('Altitude (km)')
-plt.title('Altitude of International Space Station from 21 July 2022 to 21 July 2023')
+
+# Set ticks and labels for the months
+plt.gca().xaxis.set_major_locator(MonthLocator())
+plt.gca().xaxis.set_major_formatter(DateFormatter('%b'))
+
+plt.title('Mean Altitude of International Space Station from 21 July 2022 to 21 July 2023')
 plt.grid(True)
+
 plt.show()
